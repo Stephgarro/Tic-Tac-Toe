88239431497cc58e0e8058f8e606d37df561c455
Stephgarro <109922003+Stephgarro@users.noreply.github.com>
2023-11-21 12:35:42 -0600

    feat( react): update code

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,41 +1,4 @@
 
-// import React from 'react';
-// import Square from './Square';
-
-// function Board(props) {
-//   function renderSquare(i) {
-//     return (
-//       <Square
-//         value={props.squares[i]}
-//         onClick={() => props.onClick(i)}
-//       />
-//     );
-//   }
-
- 
-//   const rows = [];
-//   for (let row = 0; row < 3; row++) {
-//     const rowSquares = [];
-//     for (let col = 0; col < 3; col++) {
-//       rowSquares.push(renderSquare(row * 3 + col));
-//     }
-//     rows.push(
-//       <div key={row} className="board-row">
-//         {rowSquares}
-//       </div>
-//     );
-//   }
-
-//   return (
-//     <div>
-//       {rows}
-//     </div>
-//   );
-// }
-
-// export default Board;
-
-
 import React from 'react';
 import Square from './Square';
 
@@ -43,7 +6,7 @@ function Board(props) {
   function renderSquare(i) {
     return (
       <Square
-        key={i} // Añade una key única
+        key={i} 
         value={props.squares[i]}
         onClick={() => props.onClick(i)}
       />
